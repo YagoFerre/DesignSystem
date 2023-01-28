eabea6fa7142477d9030c87e05f1e5dc156a7625
Yago Ferreira <yagofer12@gmail.com>
2023-01-28 14:27:49 -0300

    changesets

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   lineHeights,
   radii,
   space,
-} from '@ignite-ui/tokens'
+} from '@yago-ferreira-ignite-ui/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
